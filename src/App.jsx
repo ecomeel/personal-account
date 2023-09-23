05ec2844c6500543a6a395bd16de4ba01a4e1ce3
ecomeel <ecomeel@gmail.com>
2023-09-23 14:41:15 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header/Header"
 
 export default function App() {
   return (
     <div className="container">
-      <h1>hello</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <h1 className="header__title">Создание аккаунта</h1>
+            <p className="header__description">Введите свои данные, чтобы создать аккаунт в сервисе</p>
+        </header>
+    )
+}
