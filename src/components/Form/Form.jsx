edad84076faf96a783b493d9e6cd545357bf1d81
ecomeel <ecomeel@gmail.com>
2023-09-23 23:54:08 +0300

    checkbox

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -4,41 +4,22 @@ import Input from "../Input/Input";
 export default function Form() {
     return (
         <div className="form">
+            <Input name="name" type="text" placeholder="Имя" />
+            <Input name="surname" type="text" placeholder="Фамилия" />
+            <Input name="phone" type="tel" placeholder="Номер телефона" />
+            <Input name="email" type="email" placeholder="Email" />
+            <Input name="password" type="password" placeholder="Пароль" />
             <Input
-                name='name'
-                type='text'
-                placeholder='Имя'
+                name="check-password"
+                type="password"
+                placeholder="Повторите пароль"
             />
-            <Input
-                name='surname'
-                type='text'
-                placeholder='Фамилия'
-            />
-            <Input
-                name='phone'
-                type='tel'
-                placeholder='Номер телефона'
-            />
-            <Input
-                name='email'
-                type='email'
-                placeholder='Email'
-            />
-            <Input
-                name='password'
-                type='password'
-                placeholder='Пароль'
-            />
-            <Input
-                name='check-password'
-                type='password'
-                placeholder='Повторите пароль'
-            />
-            <label htmlFor="pass-confirmation">
-                <Input type='checkbox' name='pass-confirmation' />
-                <p>Подтверждаю пароль</p>
-            </label>
+
+            <div className="checkbox">
+                <input id="checkbox1" className="checkbox__input" type="checkbox" name="pass-confirmation" />
+                <label className="checkbox__label" htmlFor="checkbox1">Подтверждаю пароль</label>
+            </div>
 
         </div>
-    )
+    );
 }
