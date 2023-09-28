d7ad7f748a1199076c83996b373fc85b36cb8d7b
ecomeel <ecomeel@gmail.com>
2023-09-29 00:11:43 +0300

    Login layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header"
 import Form from "./components/Form/Form";
+import Login from "./components/Login/Login";
 
 export default function App() {
   return (
     <div className="container">
       <Header />
       <Form />
-
-      {/* LOGIN */}
+      <Login />
     </div>
   )
 }
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import arrowImg from "../../assets/arrow.png";
+
+
+export default function Login() {
+    return(
+        <p>Уже есть аккаунт ? <div>Войти <img src={arrowImg} /></div></p>
+    )
+}
